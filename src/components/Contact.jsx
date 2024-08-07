690572c8e65a2ed11b72a3cd6e0a71b6bcf7ae25
felipegtrujillo <felipegtrujillo@gmail.com>
2024-08-07 12:29:10 -0400

    cambios .env

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,12 +29,12 @@ const Contact = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://localhost:3060/email/send", {
+      const response = await fetch(`${import.meta.env.VITE_API_URL}`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify(form), // Usamos el objeto form directamente
+        body: JSON.stringify(form),
       });
 
       const data = await response.json();
